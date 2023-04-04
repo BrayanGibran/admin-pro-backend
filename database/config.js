a2bc2f1e12acfb0bbab7c9ce7ab62133e3a80a01
Brayan Gibran <brayan.gibran@gmail.com>
2023-04-04 17:59:16 -0600

    Fin sección 10 auth

--- database/config.js
+++ database/config.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.DB_CNN);
-    console.log('DB-Online');
+
+    console.log('DB Online');
   } catch (error) {
     console.log(error);
-    throw new Error('error a la hora de iniciar la BD');
+    throw new Error('Error a la hora de iniciar la BD ver logs');
   }
 };
 
--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
 
 const express = require('express');
-const { dbConnection } = require('./database/config');
 const cors = require('cors');
 
-//* Crear el servidor de express
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
 const app = express();
 
-//*Configurar CORS
+// Configurar CORS
 app.use(cors());
 
-//* base de datos
+// Lectura y parseo del body
+app.use(express.json());
+
+// Base de datos
 dbConnection();
 
-//cDD2vVu7FrFK1dDg
-//* Rutas
-app.get('/', (req, res) => {
-  res.status(400).json({
-    ok: true,
-    msg: 'Hola mundo',
-  });
-});
+// Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
-  console.log('Servidor corriendo en puerto ', process.env.PORT);
+  console.log('Servidor corriendo en puerto ' + process.env.PORT);
 });
